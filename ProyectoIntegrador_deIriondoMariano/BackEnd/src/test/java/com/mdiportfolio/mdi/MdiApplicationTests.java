93247a050eab55f0a8f51022f2307c61435a6c41
goldbach8 <marianodeiriondo8@gmail.com>
2023-04-11 18:36:03 -0300

    creacion backend

---
+++ ProyectoIntegrador_deIriondoMariano/BackEnd/src/test/java/com/mdiportfolio/mdi/MdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdiportfolio.mdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
