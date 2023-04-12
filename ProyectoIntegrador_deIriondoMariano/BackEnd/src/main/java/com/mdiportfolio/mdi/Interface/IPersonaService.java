254d755a4c2c7b29403a9e1ccde0d31bfb6144d6
goldbach8 <marianodeiriondo8@gmail.com>
2023-04-11 21:04:18 -0300

    avances backend

---
+++ ProyectoIntegrador_deIriondoMariano/BackEnd/src/main/java/com/mdiportfolio/mdi/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+
+package com.mdiportfolio.mdi.Interface;
+
+import com.mdiportfolio.mdi.Entity.Persona;
+import java.util.List;
+
+
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    
+   public void savePersona(Persona persona);
+   
+   public void deletePersona(Long id);
+   
+   public Persona findPersona(Long id);
+    
+}
---
+++ ProyectoIntegrador_deIriondoMariano/BackEnd/src/main/java/com/mdiportfolio/mdi/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.mdiportfolio.mdi.Repository;
+
+import com.mdiportfolio.mdi.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+    
+}
